4efde0de0d7ab2e745ba2185e75d598a9f8ff910
Lepidora <miamukherjee2+lepidora@gmail.com>
2017-12-23 00:03:41 +0000

    Added the voting module to client

--- src/client.js
+++ src/client.js
@@ -6,6 +6,7 @@
 const config = require('./config.json');
 const general = require('./client/general.js');
 const messages = require('./client/messages.js');
+const voting = require('./client/voting.js');
 
 module.exports = function(client) {
 	
@@ -13,5 +14,7 @@ module.exports = function(client) {
 	client.on('guildCreate', general.clientJoin(client, config));
 	client.on('guildDelete', general.clientLeave(client, config));
 	client.on('message', messages(client, config));
+	client.on('messageReactionAdd', voting.onReaction(client, config));
+	client.on('messageReactionRemove', voting.onReactionRemove(client, config));
 }
 
